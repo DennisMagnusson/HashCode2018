136f422982a69b9fc162850067126f3826c8a27f
Tobias GLimmerfors <tgli@kth.se>
2018-03-01 21:29:14 +0100

    alshflksa

--- SimulatorBrain.java
+++ SimulatorBrain.java
@@ -6,12 +6,16 @@ public class SimulatorBrain {
   public ArrayList<Ride> listOfRides;
   public int maxTimeSteps;
   public int timeStepsTaken = 0;
+  public int bonusReward;
 
-  public ArrayList<Integer> distancesOfRides;
+  public ArrayList<Integer> rewardsOfRides;
+  public ArrayList<Integer> timeForRides;
+  public ArrayList<Double> rewardPerTimeStep;
 
-  public SimulatorBrain(int numberOfVehicles, ArrayList<Ride> listOfRides, int maxTimeSteps) {
+  public SimulatorBrain(int numberOfVehicles, ArrayList<Ride> listOfRides, int maxTimeSteps, int bonusReward) {
     this.listOfRides = listOfRides;
     this.maxTimeSteps = maxTimeSteps;
+    this.bonusReward = bonusReward;
 
     Collections.sort(listOfRides);
 
@@ -30,19 +34,44 @@ public class SimulatorBrain {
           // ASSIGN A RIDE
           if (listOfRides.size() > 0) {
 
-            distancesOfRides = new ArrayList<Integer>();
+
+            rewardsOfRides = new ArrayList<>();
+            timeForRides = new ArrayList<>();
+            rewardPerTimeStep = new ArrayList<>();
+
             for (int i = 0; i < listOfRides.size(); i++) {
-              int distance = Math.abs(specificCar.posX - listOfRides.get(i).startX) + Math.abs(specificCar.posY - listOfRides.get(i).startY);
-              distancesOfRides.add(distance);
+              int currentTimeTaken = 0;
+              int currentReward = 0;
+
+              // getting to the starting point
+              currentTimeTaken += (Math.abs(specificCar.posX - listOfRides.get(i).startX) + Math.abs(specificCar.posY - listOfRides.get(i).startY)); // distance to start
+              if (currentTimeTaken + timeStepsTaken <= listOfRides.get(i).earliestStart) {
+                currentReward += bonusReward;
+                currentTimeTaken = listOfRides.get(i).earliestStart - timeStepsTaken;
+              }
+
+              // getting to the finishing point
+              int distanceOfTravel = Math.abs(listOfRides.goalX - listOfRides.get(i).startX) + Math.abs(listOfRides.goalY - listOfRides.get(i).startY);
+              currentTimeTaken += distanceOfTravel;
+              currentReward += distanceOfTravel;
+              if (currentTimeTaken + timeStepsTaken > maxTimeSteps) {
+                currentReward = 0;
+              }
+
+              double currentRewardPerTimeStep = (double)currentReward / (double)currentTimeTaken;
+              rewardPerTimeStep.add(currentRewardPerTimeStep);
             }
-            int lowestIndex = 0;
+
+
+            int highestIndex = 0;
             for (int i = 1; i < listOfRides.size(); i++) {
-              if (distancesOfRides.get(i) < distancesOfRides.get(lowestIndex)) {
-                lowestIndex = i;
+              if (rewardPerTimeStep.get(i) > rewardPerTimeStep.get(highestIndex)) {
+                highestIndex = i;
               }
             }
-            specificCar.assignRide(listOfRides.get(lowestIndex));
-            listOfRides.remove(lowestIndex);
+
+            specificCar.assignRide(listOfRides.get(highestIndex));
+            listOfRides.remove(highestIndex);
           }
         }
         if (specificCar.driving) {
