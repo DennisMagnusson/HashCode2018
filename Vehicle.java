231b62b48e055dc6618e59500d27d36e9ee453b8
Oscar Almqvist <oscaralmqvist@n163-p229.eduroam.kth.se>
2018-03-01 19:27:34 +0100

    asdasdasd

--- Vehicle.java
+++ Vehicle.java
@@ -1,24 +1,45 @@
 public class Vehicle {
-  public int stepsNeeded, posX, posY, earliestStart;
+  public int stepsNeeded, posX, posY, startX, startY, goalX, goalY, earliestStart;
   public boolean driving = false;
+  public boolean gettingToStart;
 
   public Vehicle() {
     posX = 0;
     posY = 0;
   }
 
-  public void assignGoal(int goalX, int goalY, int earliestStart) {
-    stepsNeeded = Math.abs(goalX - posX) + Math.abs(goalY - posY);
-    posX = goalX;
-    posY = goalY;
+  public void assignRide(int startX, int startY, int goalX, int goalY, int earliestStart) {
+    this.startX = startX;
+    this.startY = startY;
+    this.goalX = goalX;
+    this.goalY = goalY;
     this.earliestStart = earliestStart;
+
+    stepsNeeded = Math.abs(posX - startX) + Math.abs(posY - startY);
+    if (stepsNeeded == 0) {
+      stepsNeeded = Math.abs(posX - goalX) + Math.abs(posY - goalY);
+      posX = goalX;
+      posY = goalY;
+      gettingToStart = false;
+    }
+    else {
+      posX = startX;
+      posY = startY;
+      gettingToStart = true;
+    }
   }
 
 
   public void aLittleStep() {
     stepsNeeded--;
     if (stepsNeeded == 0){
-      driving = false;
+      if (gettingToStart) {
+        gettingToStart = false;
+        assignRide(startX, startY, goalX, goalY, earliestStart);
+      }
+      else {
+        driving = false;
+      }
     }
   }
 }
