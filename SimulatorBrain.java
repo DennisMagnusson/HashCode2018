789412c2d6cce88c40607c1ea3aedeee1c27b51c
Tobias GLimmerfors <tgli@kth.se>
2018-03-01 20:18:23 +0100

    llll

--- SimulatorBrain.java
+++ SimulatorBrain.java
@@ -9,6 +9,9 @@ public class SimulatorBrain {
   public SimulatorBrain(int numberOfVehicles, ArrayList<Ride> listOfRides, int maxTimeSteps) {
     this.listOfRides = listOfRides;
     this.maxTimeSteps = maxTimeSteps;
+
+    Collections.sort(listOfRides);
+
     vehicles = new Vehicle[numberOfVehicles];
     for (int i = 0; i < numberOfVehicles; i++) {
       vehicles[i] = new Vehicle();
@@ -19,7 +22,10 @@ public class SimulatorBrain {
     for (specificCar : vehicles) {
       if (!specificCar.driving) {
         // ASSIGN A RIDE
-
+        if (listOfRides.size() > 0) {
+          specificCar.assignRide(listOfRides.get(0));
+          listOfRides.remove(0);
+        }
       }
       if (specificCar.driving) {
         specificCar.aLittleStep();
