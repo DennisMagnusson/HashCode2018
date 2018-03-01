a5736de56a88723371dfa259cfc37218e76a2f61
Tobias GLimmerfors <tgli@kth.se>
2018-03-01 20:51:23 +0100

    alshflksa

--- SimulatorBrain.java
+++ SimulatorBrain.java
@@ -7,6 +7,8 @@ public class SimulatorBrain {
   public int maxTimeSteps;
   public int timeStepsTaken = 0;
 
+  public ArrayList<Integer> distancesOfRides;
+
   public SimulatorBrain(int numberOfVehicles, ArrayList<Ride> listOfRides, int maxTimeSteps) {
     this.listOfRides = listOfRides;
     this.maxTimeSteps = maxTimeSteps;
@@ -23,12 +25,27 @@ public class SimulatorBrain {
 
   public void run() {
     while (timeStepsTaken < maxTimeSteps) {
-      for (Vehicle specificCar : vehicles) {
+      for (specificCar : vehicles) {
         if (!specificCar.driving) {
           // ASSIGN A RIDE
           if (listOfRides.size() > 0) {
-            specificCar.assignRide(listOfRides.get(0));
+
+            distancesOfRides = new ArrayList<Integer>();
+            for (int i = 0; i < listOfRides.size(); i++) {
+              int distance = Math.abs(specificCar.posX - listOfRides.get(i).startX) + Math.abs(specificCar.posY - listOfRides.get(i).startY);
+              distancesOfRides.add(distance);
+            }
+            int lowestIndex = 0;
+            for (int i = 1; i < listOfRides.size(); i++) {
+              if (distancesOfRides.get(i) < distancesOfRides.get(lowestIndex)) {
+                lowestIndex = i;
+              }
+            }
+
+            //specificCar.assignRide(listOfRides.get(0));
+            specificCar.assignRide(listOfRides.get(lowestIndex));
             listOfRides.remove(0);
+
           }
         }
         if (specificCar.driving) {
