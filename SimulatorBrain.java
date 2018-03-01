afa55c75802297a49af28d505805544da509a167
Tobias GLimmerfors <tgli@kth.se>
2018-03-01 19:59:22 +0100

    added simulator brain

--- SimulatorBrain.java
+++ SimulatorBrain.java
@@ -4,9 +4,11 @@ public class SimulatorBrain {
 
   public Vehicle[] vehicles;
   public ArrayList<Ride> listOfRides;
+  public int maxTimeSteps;
 
-  public SimulatorBrain(int numberOfVehicles, ArrayList<Ride> listOfRides) {
+  public SimulatorBrain(int numberOfVehicles, ArrayList<Ride> listOfRides, int maxTimeSteps) {
     this.listOfRides = listOfRides;
+    this.maxTimeSteps = maxTimeSteps;
     vehicles = new Vehicle[numberOfVehicles];
     for (int i = 0; i < numberOfVehicles; i++) {
       vehicles[i] = new Vehicle();
@@ -17,7 +19,7 @@ public class SimulatorBrain {
     for (specificCar : vehicles) {
       if (!specificCar.driving) {
         // ASSIGN A RIDE
-        
+
       }
       if (specificCar.driving) {
         specificCar.aLittleStep();
